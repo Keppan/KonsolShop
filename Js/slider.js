641afb863c08bbf8f4882619e9d4c50953f87f80
Kevin <Kevin.willy@hotmail.com>
2016-01-27 17:15:12 +0100

    Påbörjande utav mer content.

--- Js/slider.js
+++ Js/slider.js
@@ -1,7 +1,8 @@
 $(function(){
-    $('.slideshow img:gt(0)').hide();
-    setInterval(function(){
-      $('.slideshow :first-child').fadeOut()
-         .next('img').fadeIn()
-         .end().appendTo('.slideshow');},4000);
+      $('.slideshow > :gt(0)').hide();
+  setInterval(function(){
+    var rand = Math.floor(Math.random() * ($('.slideshow').children().length-1));
+    $('.slideshow > :first-child').appendTo('.slideshow').fadeOut(1000);
+    $('.slideshow > *').eq(rand).prependTo('.slideshow').fadeIn(1000);
+  }, 4000);
 });
